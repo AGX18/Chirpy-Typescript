d031429ec164e138506500d90a6296d34379b5ec
AGX18 <agxnh2020@gmail.com>
2025-10-20 20:59:07 +0000

    initial setup is done

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { env } from './env.js'
+import app from './server.js'
+
+// Start the server
+app.listen(env.PORT, () => {
+  console.log(`Server running on port ${env.PORT}`)
+  console.log(`Environment: ${env.APP_STAGE}`)
+})
---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+// Create Express application
+const app = express()
+
+
+// Export the app for use in other modules (like tests)
+export { app }
+
+// Default export for convenience
+export default app
